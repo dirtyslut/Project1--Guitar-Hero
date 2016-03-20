3cf5a07bcbd7221a244ce1febec6ce4a04c600b2
Ahmed Abdulkarim <abdulkarim8ahmed@gmail.com>
2016-03-20 14:53:49 -0700

    button shakes

--- js/app.js
+++ js/app.js
@@ -2,4 +2,27 @@ $( document ).ready(function(){
   $('#A').keydown(function(event) {
       console.log("hello there");
   });
+
+  var a_btn = $('#A');
+
+  $('#A').click(function(){
+    $('#A').effect("shake", {times:4}, 1000);
+
+  });
+
+
 });
+
+var target=$('#target');
+target.animate({
+ width:target.width() * 2,
+ height:target.height() * 2}, 1000);
+
+
+// $('#A').addClass('animated ');
+
+
+// $( document ).click(function() {
+//   $( "#toggle" ).effect( "shake" );
+// });
+// </s
