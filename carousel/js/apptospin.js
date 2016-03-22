574f8b34bec00d252189f963aa5b92da88a425c2
Ahmed Abdulkarim <abdulkarim8ahmed@gmail.com>
2016-03-21 17:18:16 -0700

    got p5.js will paint on the canvas tonight

--- carousel/js/apptospin.js
+++ carousel/js/apptospin.js
@@ -1,47 +1,66 @@
-$( document ).ready(function() {
-  $('.container').on('click',function(){
-    console.log("hi there");
-  });
+// $( document ).ready(function() {
+//   $('body').on('click',function(){
+//     console.log("hi there");
+//   });
 
 
-  var canvas = document.getElementById('canvas');
-  var ctx = canvas.getContext('2d');
+  // var canvas = document.getElementById('canvas');
+  // var ctx = canvas.getContext('2d');
 
-  var ball = {
-    x: 100,
-    y: 100,
-    vx: 5,
-    vy: 0,
-    radius: 25,
-    color: 'blue',
-    draw: function() {
-      ctx.beginPath();
-      ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, true);
-      ctx.closePath();
-      ctx.fillStyle = this.color;
-      ctx.fill();
-    }
-  };
+  // var ball = {
+  //   x: 100,
+  //   y: 100,
+  //   vx: 0,
+  //   vy: 5,
+  //   radius: 25,
+  //   color: 'blue',
+  //   draw: function() {
+  //     ctx.beginPath();
+  //     ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, true);
+  //     ctx.closePath();
+  //     ctx.fillStyle = this.color;
+  //     ctx.fill();
+  //   }
+  // };
 
-  function draw(){
-    ctx.clearRect(0,0, canvas.width, canvas.height);
-    ball.draw();
-    ball.x += ball.vx;
-    ball.y += ball.vy;
-    raf = window.requestAnimationFrame(draw);
+  // function draw(){
+  //   ctx.clearRect(0,0, canvas.width, canvas.height);
+  //   ball.draw();
+  //   ball.x += ball.vx;
+  //   ball.y += ball.vy;
+  //   raf = window.requestAnimationFrame(draw);
+  // }
+
+
+  // canvas.addEventListener('mouseover', function(e){
+  //   raf = window.requestAnimationFrame(draw);
+  // });
+
+  // canvas.addEventListener("mouseout",function(e){
+  //   window.cancelAnimationFrame(raf);
+  // });
+
+  // for (var i = 0; i < 1; i++){
+  //   ball.draw();
+    // window.requestAnimationFrame(draw);
+
+  // }
+
+  // window.setInterval(ball.draw(), 1000);
+
+
+// });
+
+
+ function setup() {
+    createCanvas(640, 480);
   }
 
-
-  canvas.addEventListener('mouseover', function(e){
-    raf = window.requestAnimationFrame(draw);
-  });
-
-  canvas.addEventListener("mouseout",function(e){
-    window.cancelAnimationFrame(raf);
-  });
-
-
-
-  window.setInterval(ball.draw(), 1000);
-
-});
+  function draw() {
+    if (mouseIsPressed) {
+      fill(0);
+    } else {
+      fill(255);
+    }
+    ellipse(mouseX, mouseY, 80, 80);
+  }
